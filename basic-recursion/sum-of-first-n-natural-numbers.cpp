56698e2c65cab42b99b31e89bacc8dd3247e8b40
Ravi <ravipkakadia1@gmail.com>
2025-07-02 16:18:11 -0700

    Sum of first n natural numbers

---
+++ basic-recursion/sum-of-first-n-natural-numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int recursion(int n){
+    if(n==1) return 1;
+    else return n + recursion(n-1);
+}
+
+int main(){
+
+    int n=5;
+    
+    cout<<recursion(n);
+
+    return 0;
+}
